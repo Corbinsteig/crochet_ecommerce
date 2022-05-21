5d9f61230e7062e3fc30131cf78af98a86c55a78
Corbin Steig <corsteig1@gmail.com>
2022-05-21 01:52:45 -0700

    :)

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,7 @@ export default function Home() {
       </Head>
       <div className="navCart">
       <a href="#/cart" id="cart_home">
-        <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" className="snipcart-cart-header__icon snipcart__icon"  >
-        <path fillRule="evenodd" clipRule="evenodd" d="M51.714 20.47L55 60H9l3.286-39.53h9.857v-6.588C22.143 8.424 26.556 4 32 4c5.444 0 9.857 4.424 9.857 9.882v6.589h9.857zM25.43
-         13.883v16.47h-3.286v-6.587h-6.834l-2.737 32.94h38.856l-2.737-32.94h-6.834v6.588h-3.286v-16.47c0-3.634-2.947-6.589-6.571-6.589-3.624 0-6.571 2.955-6.571 6.588zm3.285 9.883V20.47h6.572v3.294h-6.572z" fill="pink"></path>
-        </svg>
+        
         </a>
       </div>
       <main className={styles.main}>
@@ -29,7 +26,10 @@ export default function Home() {
         
         <p className={styles.description}>
         <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
-            <strong>Cart:</strong> <span className="snipcart-total-price">$0.00</span>
+        <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" className="snipcart-cart-header__icon snipcart__icon"  >
+        <path fillRule="evenodd" clipRule="evenodd" d="M51.714 20.47L55 60H9l3.286-39.53h9.857v-6.588C22.143 8.424 26.556 4 32 4c5.444 0 9.857 4.424 9.857 9.882v6.589h9.857zM25.43
+         13.883v16.47h-3.286v-6.587h-6.834l-2.737 32.94h38.856l-2.737-32.94h-6.834v6.588h-3.286v-16.47c0-3.634-2.947-6.589-6.571-6.589-3.624 0-6.571 2.955-6.571 6.588zm3.285 9.883V20.47h6.572v3.294h-6.572z" fill="pink"></path>
+        </svg> <span className="snipcart-total-price">$0.00</span>
           </a>
         </p>
 
